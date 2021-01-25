4f4d5e1975b83358d3799051e005f5a309f2d499
David Gonzalez <davidgonzalez0519@gmail.com>
2021-01-25 13:43:28 -0500

    star project
    First commit added templates Home and Header

---
+++ src/pages/templates/Header.js
@@ -0,0 +1,21 @@
+const Header = () => {
+  const view = `
+   <div class="Header">
+      <div class"Header-logo>
+         <h1>
+            <a href="/">
+               100tifi.co
+            </a>
+         </h1>
+      </div>
+      <div class="Header-nav">
+         <a href="#/abaut/>
+            About
+         </a>
+      </div>
+   </div>
+   `;
+  return view;
+};
+
+export default Header;
